128d300300555e9a33a2f2a4fbe89814cf6b4be5
Jakub Nowaczewski <nowacja_aehit@students.vizja.pl>
2025-12-07 14:00:11 +0100

    Upgrade project to use `Java 21` using openrewrite.

--- src/main/java/com/mycompany/sapo_leyendo/database/SqlScriptRunner.java
+++ src/main/java/com/mycompany/sapo_leyendo/database/SqlScriptRunner.java
@@ -3,7 +3,7 @@ package com.mycompany.sapo_leyendo.database;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -28,7 +28,7 @@ public class SqlScriptRunner {
     public static void executeScript(Connection conn, String scriptFilePath) throws SQLException, IOException {
         
         System.out.println("Wykonywanie skryptu: " + scriptFilePath);
-        String sql = new String(Files.readAllBytes(Paths.get(scriptFilePath)), StandardCharsets.UTF_8);
+        String sql = new String(Files.readAllBytes(Path.of(scriptFilePath)), StandardCharsets.UTF_8);
 
         // Usuwamy komentarze jednoliniowe (ważne dla SQLite)
         // Ta linia jest nadal potrzebna, aby usunąć komentarze na końcu linii (np. "INSERT ...; -- komentarz")
@@ -92,7 +92,7 @@ public class SqlScriptRunner {
             try {
                 executeScriptFromResource(conn, tempPath);
             } finally {
-                Files.deleteIfExists(Paths.get(tempPath));
+                Files.deleteIfExists(Path.of(tempPath));
             }
         }
     }
